7e78a3778b2066d159e763a1df36f4e0a8bb50a5
MartMbithi <martdevelopers254@gmail.com>
2021-01-26 10:09:25 +0300

    Refactored superfluous legacy codebase

--- resources/views/Modules/Admin/ohcms_admin_pages_add_outpatient_vitals.php
+++ resources/views/Modules/Admin/ohcms_admin_pages_add_outpatient_vitals.php
@@ -3,23 +3,24 @@ session_start();
 include('assets/configs/config.php');
 include('assets/configs/checklogin.php');
 check_login();
-$aid=$_SESSION['admin_id'];
+$aid = $_SESSION['admin_id'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  
-<?php include('includes/header.php');?>
-  <body>
-    <div class="be-wrapper">
-      <!--Navbar-->
-      <?php include('includes/navbar.php');?>
-        <!--Sidebarbar-->
-      <?php include('includes/sidebar.php');?>
 
-      <div class="be-content">
-        <div class="main-content container-fluid">
+<?php include('includes/header.php'); ?>
+
+<body>
+  <div class="be-wrapper">
+    <!--Navbar-->
+    <?php include('includes/navbar.php'); ?>
+    <!--Sidebarbar-->
+    <?php include('includes/sidebar.php'); ?>
+
+    <div class="be-content">
+      <div class="main-content container-fluid">
         <div class="row">
-            <div class="col-sm-12">
+          <div class="col-sm-12">
             <nav aria-label="breadcrumb">
               <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="ohcms_pages_admin_dashboard.php">Dashboard</a></li>
@@ -27,60 +28,60 @@ $aid=$_SESSION['admin_id'];
                 <li class="breadcrumb-item active" aria-current="page">Out Patient Record</li>
               </ol>
             </nav>
-              <div class="card card-table">
-                <div class="card-header">Out Patients Records
-                  <div class="tools dropdown">
-                    <div class="dropdown-menu" role="menu">
-                      <div class="dropdown-divider"></div>
-                    </div>
+            <div class="card card-table">
+              <div class="card-header">Out Patients Records
+                <div class="tools dropdown">
+                  <div class="dropdown-menu" role="menu">
+                    <div class="dropdown-divider"></div>
                   </div>
                 </div>
-                <div class="card-body">
-                  <table class="table table-striped table-hover table-fw-widget" id="table1">
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Address</th>
-                        <th>Registration Date</th>
-                        <th>Action</th>
-                      </tr>
-                    </thead>
-                    <?php
-                        
-                        $ret="SELECT * FROM patients Where p_type = 'OutPatient' ";
-                        $stmt= $mysqli->prepare($ret) ;
-                        //$stmt->bind_param('i',$aid);
-                        $stmt->execute() ;//ok
-                        $res=$stmt->get_result();
-                        $cnt=1;
-                        while($row=$res->fetch_object())
-                          {
-                    ?>
-                    <tbody>
-                    <tr class="odd gradeX even gradeC odd gradeA ">
-                        <td><?php echo $cnt;?></td>
-                        <td><?php echo $row->p_fname;?> <?php echo $row->p_lname;?></td>
-                        <td><?php echo $row->p_age;?></td>
-                        <td><?php echo $row->p_address;?></td>
-                        <td class="center"><?php echo $row->created_at;?></td>
-                        <td>
-                            <a class="badge badge-primary" href='ohcms_admin_pages_capture_out_patient_vitals.php?p_id=<?php echo $row->p_id;?>'><i  class="mdi mdi-check-circle"></i> Capture Vitals</a>
-                            <a class="badge badge-success" href='ohcms_admin_pages_view_out_patient_vitals.php?p_id=<?php echo $row->p_id;?>'><i  class="mdi mdi-eye-check-outline"></i> View Vitals</a>
+              </div>
+              <div class="card-body">
+                <table class="table table-striped table-hover table-fw-widget" id="table1">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Age</th>
+                      <th>Address</th>
+                      <th>Registration Date</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <?php
 
-                        </td> 
+                  $ret = "SELECT * FROM patients Where p_type = 'OutPatient' ";
+                  $stmt = $mysqli->prepare($ret);
+                  //$stmt->bind_param('i',$aid);
+                  $stmt->execute(); //ok
+                  $res = $stmt->get_result();
+                  $cnt = 1;
+                  while ($row = $res->fetch_object()) {
+                  ?>
+                    <tbody>
+                      <tr class="odd gradeX even gradeC odd gradeA ">
+                        <td><?php echo $cnt; ?></td>
+                        <td><?php echo $row->p_fname; ?> <?php echo $row->p_lname; ?></td>
+                        <td><?php echo $row->p_age; ?></td>
+                        <td><?php echo $row->p_address; ?></td>
+                        <td class="center"><?php echo $row->created_at; ?></td>
+                        <td>
+                          <a class="badge badge-primary" href='ohcms_admin_pages_capture_out_patient_vitals.php?p_id=<?php echo $row->p_id; ?>'><i class="mdi mdi-check-circle"></i> Capture Vitals</a>
+                          <a class="badge badge-success" href='ohcms_admin_pages_view_out_patient_vitals.php?p_id=<?php echo $row->p_id; ?>'><i class="mdi mdi-eye-check-outline"></i> View Vitals</a>
+
+                        </td>
                       </tr>
                     </tbody>
-                  <?php $cnt=$cnt+1; }?>
+                  <?php $cnt = $cnt + 1;
+                  } ?>
 
-                  </table>
-                </div>
+                </table>
               </div>
             </div>
           </div>
+        </div>
       </div>
-      
+
     </div>
     <script src="assets/lib/jquery/jquery.min.js" type="text/javascript"></script>
     <script src="assets/lib/perfect-scrollbar/js/perfect-scrollbar.min.js" type="text/javascript"></script>
@@ -100,12 +101,12 @@ $aid=$_SESSION['admin_id'];
     <script src="assets/lib/datatables/datatables.net-responsive/js/dataTables.responsive.min.js" type="text/javascript"></script>
     <script src="assets/lib/datatables/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js" type="text/javascript"></script>
     <script type="text/javascript">
-      $(document).ready(function(){
-      	//-initialize the javascript
-      	App.init();
-      	App.dataTables();
+      $(document).ready(function() {
+        //-initialize the javascript
+        App.init();
+        App.dataTables();
       });
     </script>
-  </body>
+</body>
 
 </html>
